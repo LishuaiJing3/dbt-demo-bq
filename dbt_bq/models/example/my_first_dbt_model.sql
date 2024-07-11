c8c4200f69e33e0e15812d045713614b310b6a2c
lishuai <lishuai.jing@gmail.com>
2024-07-11 15:38:23 +0200

    feat: up and running

---
+++ dbt_bq/models/example/my_first_dbt_model.sql
@@ -0,0 +1,40 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+-- models/create_users_products_table.sql
+
+{{ config(materialized='table') }}
+
+WITH source_data AS (
+    SELECT
+        1 AS userid,
+        101 AS productid,
+        'Male' AS usergender
+    UNION ALL
+    SELECT
+        2 AS userid,
+        102 AS productid,
+        'Female' AS usergender
+    UNION ALL
+    SELECT
+        3 AS userid,
+        103 AS productid,
+        'Male' AS usergender
+)
+
+SELECT *
+FROM source_data
+
+
+
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
